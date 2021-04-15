9836bf9f155f2a3e42b619669291dfb66a7a9d89
Kaden Storrs <kadenstorrs@gmail.com>
2021-04-15 15:08:22 -0600

    Initial Commit

---
+++ Shared/FugitiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  FugitiveApp.swift
+//  Shared
+//
+//  Created by Kaden Storrs on 4/15/21.
+//
+
+import SwiftUI
+
+@main
+struct FugitiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
